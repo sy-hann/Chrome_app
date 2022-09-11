1711e387e9931de97a64e80cdaf55eab1afaa17b
thedeveloper23 <seoyon0523@gmail.com>
2022-09-12 01:50:47 +0900

    Update background.js

--- js/background.js
+++ js/background.js
@@ -2,4 +2,4 @@ const images = ["0.jpeg","1.jpeg","2.jpeg"];
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 const bgImage = document.querySelector("body");
 
-bgImage.style.backgroundImage = `url(/img/${chosenImage})`;
+bgImage.style.backgroundImage = `url(./img/${chosenImage})`;
